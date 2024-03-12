c4805cb2d3fd90959f0fa9f1b2621dde315fcb2f
Tendry24 <hei.tendry.2@gmail.com>
2024-03-12 19:52:43 +0300

    feat: add Swagger specification

---
+++ src/main/java/com/digital/bank/util/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.digital.bank.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private Integer status;
+    private Object payload;
+    private String message;
+}
