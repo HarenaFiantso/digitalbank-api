4acadad117924c6dd90127ba1b9d33c8370b6e2e
HarenaFiantso <fiantsorav@gmail.com>
2024-03-12 10:58:11 +0300

    Initial commit from Spring initializr

---
+++ src/main/java/com/numeric/bank/NumericbankApplication.java
@@ -0,0 +1,13 @@
+package com.numeric.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumericbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumericbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numeric/bank/NumericbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numeric.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumericbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
