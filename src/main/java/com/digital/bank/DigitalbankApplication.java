30834105611a3fac97f8908f948ed64d7b8fc441
tanjonaaa <127602061+tanjonaaa@users.noreply.github.com>
2024-03-16 12:02:20 +0300

    chore: added flyway for db migations
    * chore: added flyway for database migration
    
    * chore: get database connection from app properties
    
    * chore: run migrations on application startup

--- src/main/java/com/digital/bank/DigitalbankApplication.java
+++ src/main/java/com/digital/bank/DigitalbankApplication.java
@@ -1,13 +1,33 @@
 package com.digital.bank;
 
+import com.digital.bank.util.database.DbProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+
 @SpringBootApplication
 public class DigitalbankApplication {
+
+	@Autowired
+	private DbProperties dbProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalbankApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void migrateOnStartup() {
+		Flyway flyway = Flyway.configure()
+				.dataSource(
+						dbProperties.getUrl(),
+						dbProperties.getUser(),
+						dbProperties.getPassword())
+				.load();
+		flyway.migrate();
+	}
 }
