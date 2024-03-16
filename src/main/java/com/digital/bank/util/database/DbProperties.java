3e8eb6d32eb2b2f97240ae7479488f2ebf2d0768
tanjonaaa <hei.tanjona@gmail.com>
2024-03-16 11:57:11 +0300

    chore: get database connection from app properties

---
+++ src/main/java/com/digital/bank/util/database/DbProperties.java
@@ -0,0 +1,18 @@
+package com.digital.bank.util.database;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+@ConfigurationProperties("db")
+public class DbProperties {
+    private String url;
+    private String user;
+    private String password;
+}
